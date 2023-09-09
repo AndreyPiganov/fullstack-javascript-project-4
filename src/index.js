2f13b96678110f25fddeab3dde79091c5356ecc5
AndreyPiganov <andrey.piganov@mail.ru>
2023-09-09 13:21:06 +0300

    test

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ const pageLoader = (inputUrl, output = '') => {
         if (isNotOriginHostUrl(oldSrc, url) || oldSrc === undefined) {
           return 
         }
-          const elUrl = new URL(url.origin + path.join(url.pathname, oldSrc));
+          const elUrl = new URL(path.join(url.href, oldSrc));
           const extname = oldSrc.match(/(\.\w+)(?=\?|$)/i) || '';
           const elementPath = `${fileName}-${oldSrc.replace(extname[0], '').split(/[?_/]/).join('-')}${extname[0]}`;
           const absoluteElementPath = getAbsoluteFilePath(absoluteDirPath, elementPath);
